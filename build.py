b7bbb353145c9b50ffd090bf1b85def74f1c8133
Felix Borchers <felix.borchers@immobilienscout24.de>
2017-10-15 16:22:17 +0200

    Add version tag to build

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
+use_plugin("python.coverage")
 use_plugin("python.distutils")
 
 
@@ -13,6 +14,12 @@ default_task = ['analyze', 'publish']
 
 @init
 def set_properties(project):
-    #project.depends_on('flask')
+    project.depends_on('git')
+    from git import Repo
+    last_tag = Repo().git.tag().split()[-1]
+    current_tag = Repo().git.describe('--tags')
+    project.version = last_tag
+    if last_tag != current_tag:
+        project.version = project.version + 'dev'
     project.set_property('flake8_ignore', 'E402')
     project.set_property('flake8_verbose_output', True)
